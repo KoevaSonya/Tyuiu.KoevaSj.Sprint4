521a34f98447535974d496b9eb452a583c8138e1
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-04 18:50:24 +0500

    Task2

---
+++ Tyuiu.KoevaSJ.Sprint4.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoevaSJ.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint4.Task2.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[] sumArray = { 3, 5, 6 , 8, 4, 5, 7, 8, 3, 4, 5, 6 ,7 , 8, 3 };
+            int res = ds.Calculate(sumArray);
+            int wait = 44;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KoevaSJ.Sprint4.Task2.V11/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KoevaSJ.Sprint4.Task2.V11.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint4.Task2.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Коева С.Ю. | АСОиУб-23-1 ";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                    *");
+            Console.WriteLine("* Одномерные массивы (статический ввод)                                        *");
+            Console.WriteLine("* Задание #2                                                                   *");
+            Console.WriteLine("* Вариант #11                                                                   *");
+            Console.WriteLine("* Выполнил: Коева Софья Юрьевна | АСОиУб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 15 элементов заполненный значениями   *");
+            Console.WriteLine("* в диапазоне от 3 до 8 подсчитать cумму четных элементов массива*       ");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            int len;
+            Console.Write("Введите колличество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] sumArray = new int[len];
+
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write("Введите значение " + i + "элемента массива: ");
+                sumArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine(sumArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("Сумма четных элементов массива: ");
+
+            int res = ds.Calculate(sumArray);
+
+
+            Console.Write(res);
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}

fe21e104982e6de06f55677a6fa3343567df2ea2
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-12 00:00:10 +0500

    Task5

---
+++ Tyuiu.KoevaSJ.Sprint4.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoevaSJ.Sprint4.Task5.V26.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint4.Task5.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { -4, 2, -3, 1, 0 },
+                                          { -2, -2, -4, 2, 2 },
+                                          { 1, 1, 2, -4, -3 },
+                                          { -2, -3, -4, 2, 0 },
+                                          { 1, 2, 2, -4, 2 } };
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = new int[5, 5] {  { -4, 1, -3, 1, 0 },
+                                          { -2, -2, -4, 1, 1 },
+                                          { 1, 1, 1, -4, -3 },
+                                          { -2, -3, -4, 1, 0 },
+                                          { 1, 1, 1, -4, 1 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KoevaSJ.Sprint4.Task5.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint4.Task5.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint4.Task5.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7cd9b9d-795f-4961-a172-4ac83ffeebd7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
